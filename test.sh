71341c5771d38396af2b574404c64277c3c9533d
laihj <laihj@servyou.com.cn>
2018-07-03 15:46:09 +0800

    在父shell的环境中一同运行

--- test.sh
+++ test.sh
@@ -5,22 +5,6 @@ CATALINA_HOME="/d/progammer/apache-tomcat-7.0.52"
 
 JAVA_OPTS="";
 
-if [ -r "$CATALINA_BASE"/conf/findAgent.sh ]; then
-    AGENT_OPTS=. "$CATALINA_BASE"/conf/findAgent.sh "$CATALINA_BASE"/conf/server.xml "$CATALINA_HOME"/webapps
-    if [ "$AGENT_OPTS" != "" ]; then {
-      AGENT_OPTS=$(echo $AGENT_OPTS | awk -F\!\!\!\!\! '{print $NF}')
-      echo $AGENT_OPTS
-      if [ "$AGENT_OPTS" != "" ]; then {
-        JAVA_OPTS="$JAVA_OPTS $AGENT_OPTS"
-      }
-      else {
-          echo "Can't find AGENT_OPTS"
-          echo "ignore"
-      }
-      fi
-    }
-    fi
-else
-    echo "Cannot find $CATALINA_BASE/conf/findAgent.sh"
-    echo "ignore"
-fi
+. "$CATALINA_BASE"/bin/findAgent.sh "$CATALINA_BASE"/conf/server.xml "$CATALINA_HOME"/webapps
+
+echo $JAVA_OPTS
